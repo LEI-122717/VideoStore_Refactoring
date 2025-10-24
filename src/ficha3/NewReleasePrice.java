fdffa1fd53011e0b761578505725ae1453b7a31b
LEI-122666 <nttns@iscte-iul.pt>
2025-10-24 16:20:38 +0100

    V8 para V9

---
+++ src/ficha3/NewReleasePrice.java
@@ -0,0 +1,22 @@
+package ficha3;
+
+import v9.Price;
+
+public class NewReleasePrice extends Price
+{
+
+	public NewReleasePrice()
+	{
+	}
+
+	public double getRentalAmount(int duration)
+	{
+		return duration * 3;
+	}
+
+	public int getFrequentRentalPoints(int duration)
+	{
+		return duration > 1 ? 2 : 1;
+	}
+
+}
